71929f4bb4f1531790c46b0a0177ad859125a420
Mauro Risso <73178418+y0mauro@users.noreply.github.com>
2024-10-05 15:17:19 +0200

    refactor: Update page content to reflect Carbonne's features accurately

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,9 @@ export default function Home() {
         <div>
           <h2 className="text-4xl font-bold mb-6">Why Carbonne?</h2>
           <p className="text-xl mb-8">
-            Carbonne is a platform that helps you create intelligent
-            questionnaires, gather essential information about your customers
-            and speed-up your freelance and consulting projects. All in one
-            place.
+            Carbonne is a platform that helps you create intelligent forms,
+            gather essential information about your customers and speed-up your
+            freelance and consulting projects. All in one place.
           </p>
         </div>
       </Section>
